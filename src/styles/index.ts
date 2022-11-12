b3b17d051406aeb9f98bfa6c66fe4ac9b5bee22a
gabriel-78 <gabriel.78.rodrigues@gmail.com>
2022-11-12 19:04:29 -0300

    CONFIG: Config global styles

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -3,7 +3,15 @@ import {createStitches} from "@stitches/react";
 export const {config,createTheme,getCssText,globalCss,keyframes,styled,theme,css} = createStitches({
     theme: {
         colors: {
-            rocketseat: "#8257E6",
+            white: "#FFF",
+
+            gray900: "#121214",
+            gray800: "#202024",
+            gray300: "#C4C4CC",
+            gray100: "#E1E1E6",
+
+            green500: "#00875f",
+            green300: "#00B37E",
         }
     }
 });
